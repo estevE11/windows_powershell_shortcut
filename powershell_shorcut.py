a80da74108c519e6a3196624dde1410538d07a6f
Roger Esteve <Coconaut7@gmail.com>
2019-10-27 21:54:32 +0100

    PS opens, but can't find a way to make the shorcut custom.

--- powershell_shorcut.py
+++ powershell_shorcut.py
@@ -1,21 +1,36 @@
 import sys
+import subprocess
+from pynput import keyboard
+
+keys = {}
+ps_dir = "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
 
 def main():
     if len(sys.argv) < 3:
         return
-    
-    ps_dir = "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
+
     if sys.argv[1] != "none":
         ps_dir = sys.argv[1]
 
-    keys = []
     for i in range(len(sys.argv)-2):
-        keys.append(sys.argv[i])
+        keys[sys.argv[i+2]] = False
+    print(keys)
 
-    
+    with keyboard.Listener(on_press=key_pressed, on_release=key_released) as listener:
+        listener.join()
 
-    
+def key_pressed(key):
+    print('{0} pressed'.format(key))
+    keys[str(key)] = True
+    run = True
+    for key in keys:
+        if keys[key] == False:
+            run = False
+    if run:
+        subprocess.call(ps_dir)
 
+def key_released(key):
+    print('{0} pressed'.format(key))
+    keys[str(key)] = False
 
-if __name__ == "main":
-    main()
+main()
